b88873c0778544b4db164952c32c80374ab95137
Donat <donat_kolzenov@mail.ru>
2022-10-14 00:04:57 +0700

    add: sqlite context extension

---
+++ samples/EFCoreSample.Api/Extensions/SqliteContextExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace EFCoreSample.Api.Extensions
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using EFCoreSample.Data;
+
+    public static class SqliteContextExtension
+    {
+        private const string ConnectionStringName = "SqliteConnection";
+
+        public static void AddCustomSqliteContext(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration
+                .GetConnectionString(ConnectionStringName);
+
+            services.AddDbContext<ApplicationDbContext>(
+                o => o.UseSqlite(connectionString));
+
+            services.BuildServiceProvider()
+                .GetService<ApplicationDbContext>()!
+                .Database
+                .Migrate();
+        }
+    }
+}
+
--- samples/EFCoreSample.Api/Program.cs
+++ samples/EFCoreSample.Api/Program.cs
@@ -1,8 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using EFCoreSample.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCustomSqliteContext(builder.Configuration);
 
 var app = builder.Build();
 

bf97548d6a9cf9d923c070f099db16c63f777cb7
Donat <donat_kolzenov@mail.ru>
2022-10-14 10:47:02 +0700

    add: postgresql context extension

--- samples/EFCoreSample.Api/Program.cs
+++ samples/EFCoreSample.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddCustomSqliteContext(builder.Configuration);
+builder.Services.AddCustomPostgreSqlContext(builder.Configuration);
 
 var app = builder.Build();
 

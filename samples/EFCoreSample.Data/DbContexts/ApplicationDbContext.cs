2b5862507aa8c9d911d06ce439626543f2349ebd
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-10-22 13:54:16 +0700

    add: WordSet entity

--- samples/EFCoreSample.Data/DbContexts/ApplicationDbContext.cs
+++ samples/EFCoreSample.Data/DbContexts/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 namespace EFCoreSample.Data.DbContexts
 {
     using Microsoft.EntityFrameworkCore;
+    
+    using  EFCoreSample.Data.Entities;
 
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<WordSet> WordSets { get; set; } = null!;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
     }
 }
-
---
+++ samples/EFCoreSample.Data/Entities/WordSet.cs
@@ -0,0 +1,10 @@
+namespace EFCoreSample.Data.Entities;
+
+public class WordSet
+{
+    public long Id { get; set; }
+
+    public string Words { get; set; } = null!;
+
+    public string Category { get; set; } = null!;
+}

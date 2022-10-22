df8df7166e53c7583e6953989d2d9be7bb390673
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-10-22 13:48:37 +0700

    remove: test entity

--- samples/EFCoreSample.Data/DbContexts/ApplicationDbContext.cs
+++ samples/EFCoreSample.Data/DbContexts/ApplicationDbContext.cs
@@ -2,12 +2,8 @@ namespace EFCoreSample.Data.DbContexts
 {
     using Microsoft.EntityFrameworkCore;
 
-    using EFCoreSample.Data.Entities;
-
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<TestEntity> Tests { get; set; } = null!;
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {

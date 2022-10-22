43358a290ab8c0ed6020e4263f17a57551d3d499
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-10-22 15:59:29 +0700

    add: alphabet controller

---
+++ samples/EFCoreSample.Api/Controllers/AlphabetController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EFCoreSample.Data.DbContexts;
+using EFCoreSample.Data.Entities;
+
+namespace EFCoreSample.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AlphabetController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public AlphabetController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<List<Alphabet>> GetAllAsync()
+    {
+        var alphabets = await _context.Alphabets.AsNoTracking().ToListAsync();
+
+        return alphabets;
+    }
+    
+    [HttpPost]
+    public async Task<Alphabet> AddAsync(Alphabet alphabet)
+    {
+        _context.Alphabets.Add(alphabet);
+        await _context.SaveChangesAsync();
+
+        return alphabet;
+    }
+
+    [HttpPut]
+    public async Task<Alphabet> UpdateAsync(Alphabet alphabet)
+    {
+        _context.Alphabets.Update(alphabet);
+        await _context.SaveChangesAsync();
+
+        return alphabet;
+    }
+
+    [HttpDelete]
+    public async Task<Alphabet> RemoveAsync(Alphabet alphabet)
+    {
+        _context.Remove(alphabet);
+        await _context.SaveChangesAsync();
+
+        return alphabet;
+    }
+}

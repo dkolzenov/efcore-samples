7ac8e83c1c9fb23570587a49213d5131699b5c45
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-10-30 18:16:06 +0700

    update: sqlite migrations

---
+++ samples/EFCoreSample.Persistence.Sqlite/Migrations/20221030111338_Initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreSample.Persistence.Sqlite.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Samples",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 400, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 400, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samples", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Samples",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[] { 1L, "SampleDescription_1", "SampleTitle_1" });
+
+            migrationBuilder.InsertData(
+                table: "Samples",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[] { 2L, "SampleDescription_2", "SampleTitle_2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Samples");
+        }
+    }
+}
